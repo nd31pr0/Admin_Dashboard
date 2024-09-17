07d7415d5265d480f7f474163015baad849fb3ef
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-17 15:30:07 +0100

    render subscription compo info

--- src/components/Subscriptions/Subscriptions.jsx
+++ src/components/Subscriptions/Subscriptions.jsx
@@ -1,8 +1,37 @@
-import './Subscriptions';
+import './Subscriptions.css';
+import {subscriptions} from '../../data/data';
+import { iconsImgs } from '../../utils/images';
 
 function Subscriptions() {
   return (
-    <div>Subscriptions</div>
+    <div className='subgrid-two-item grid-common grid-c5'>
+        <div className='grid-c-title'>
+            <h3 className='grid-c-title-text'>Subscriptions</h3>
+            <button className='grid-c-title-icon'>
+                <img src={iconsImgs.plus} />
+            </button>
+        </div>
+        <div className='grid-c5-content'>
+            <div className='grid-items'>
+                {
+                    subscriptions.map((subscription) => (
+                        <div className='grid-item' key = {subscription.id}>
+                            <div className='grid-item-l'>
+                                <div className='icon'>
+                                    <img src={iconsImgs.alert}/>
+                                </div>
+                                <div className='text text-silver-v1'>{subscription.title}
+                                <span>due {subscription.due_date}</span></div>
+                            </div>
+                            <div className='grid-item-r'>
+                                <span className='text-silver-v1'>${subscription.amount}</span>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 
