ba74c60c4d1bddecfe334e09a99f8e145172b25c
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-17 10:21:15 +0100

    Add transactions card

--- src/components/contentMain/ContentMain.jsx
+++ src/components/contentMain/ContentMain.jsx
@@ -1,11 +1,13 @@
 import './ContentMain.css';
 import Cards from '../Cards/Cards'
+import Transactions from '../Transactions/Transactions';
 
 const ContentMain = () => {
   return (
     <div className='main-content-holder'>
         <div className='content-grid-one'>
             <Cards />
+            <Transactions />
         </div>
     </div>
   )
