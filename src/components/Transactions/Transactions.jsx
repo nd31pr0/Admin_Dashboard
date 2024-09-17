0c80009f7d54b0213a297ca1ace322190c0b7afc
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-17 10:33:41 +0100

    add images to transaction card

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -7,6 +7,24 @@ function Transactions() {
     <div className='grid-one-item grid-common grid-c2'>
         <div className='grid-c-title'>
             <h3 className='grid-c-title-text'>All Transactions</h3>
+            <button className='grid-c-title-icon'>
+                <img src={iconsImgs.plus} />
+            </button>
+        </div>
+        <div className='grid-content'>
+            <div className='grid-items'>
+                {
+                    transactions.map((transaction) => (
+                        <div className='grid-item' key={transaction.id}>
+                            <div className='grid-item-l'>
+                                <div className='avatar img-fit-cover'>
+                                    <img src={transaction.image} />
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     </div>
   )
