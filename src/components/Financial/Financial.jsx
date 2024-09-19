7940d97854c3158e87e6d08f693fd6732b3c1487
Ndzi Nfor <ndzinfor@gmail.com>
2024-09-19 07:51:50 +0100

    write Financial compo

--- src/components/Financial/Financial.jsx
+++ src/components/Financial/Financial.jsx
@@ -1,9 +1,19 @@
 import './Financial.css';
+import { iconsImgs } from '../../utils/images';
 
 function Financial() {
   return (
-    <div>
-    
+    <div className='subgrid-two-item grid-common grid-c8'>
+      <div className='grid-c-title'>
+        <h3 className='grid-c-title-text'>Financial Advice</h3>
+        <button className='grid-c-title-icon' type='button'>
+          <img src={iconsImgs.plus}/>
+        </button>
+      </div>
+      <div className='grid-c8-content'>
+        <p className='text text-silver-v1'>Ipsum dolor sit amet consectetur, 
+        adispiscing elit. iste, vitae ...</p>
+      </div>
     </div>
   )
 }
